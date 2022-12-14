63f02a456e0dec75d61d00efdb7a3d45ff4ae6b3
avric995 <avricmiladin995@gmail.com>
2022-12-15 00:54:49 +0100

    created CourseCard component with some functionality and appropriate css file

---
+++ src/components/Courses/Components/CourseCard/CourseCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './courseCard.css';
+import Button from '../../../../common/Button/Button';
+import time_convert from '../../../../helpers/pripeDuration';
+
+const CourseCard = ({
+	id,
+	title,
+	description,
+	creationDate,
+	duration,
+	authors,
+}) => {
+	return (
+		<>
+			<article key={id} className='course'>
+				<div className='flex-1'>
+					<h3>{title}</h3>
+					<p>{description}</p>
+				</div>
+				<div className='flex-2'>
+					{/* spread operator <h4>Authors: {...authors}</h4> */}
+					<h4>Authors: {}</h4>
+					<h4>
+						Duration: <span>{time_convert(duration)}</span>{' '}
+					</h4>
+					<h4>
+						Created: <span> {creationDate} </span>
+					</h4>
+					<Button value='Show course' />
+				</div>
+			</article>
+		</>
+	);
+};
+
+export default CourseCard;
