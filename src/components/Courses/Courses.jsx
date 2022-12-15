58cc6b255dbded6a07729e8edcb40b647303dd00
avric995 <avricmiladin995@gmail.com>
2022-12-15 01:00:25 +0100

    added Course.jsx component and Course.css with some functionality

---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import CourseCard from './Components/CourseCard/CourseCard';
+import { mockedCoursesList, mockedAuthorsList } from '../../constants.js';
+import './courses.css';
+import SearchBar from './Components/SearchBar/SearchBar';
+import Button from '../../common/Button/Button';
+
+const Courses = () => {
+	const [courses, setCourses] = useState(mockedCoursesList);
+	const [authors, setAuthors] = useState(mockedAuthorsList);
+
+	const [query, setQuery] = useState('');
+	// console.log(query);
+
+	// proba za search
+	const newCourses = courses.filter((course) => {
+		if (query === '') {
+			return course;
+		} else if (
+			course.title.toLowerCase().includes(query.toLowerCase()) ||
+			course.id.toLowerCase().includes(query.toLowerCase())
+		) {
+			return course;
+		}
+	});
+
+	// console.log(newCourses);
+
+	return (
+		<main>
+			<section className='container'>
+				<div className='section-element'>
+					<SearchBar
+						placeholderText='Enter course name or id...'
+						onChange={(event) => setQuery(event.target.value)}
+						query={query}
+					/>
+					<Button value='Add new course' />
+				</div>
+				<div>
+					{courses
+						.filter((course) => {
+							if (query === '') {
+								return course;
+							} else if (
+								course.title.toLowerCase().includes(query.toLowerCase()) ||
+								course.id.toLowerCase().includes(query.toLowerCase())
+							) {
+								return course;
+							}
+						})
+						.map((course) => {
+							return <CourseCard key={course.id} {...course} />;
+						})}
+				</div>
+			</section>
+		</main>
+	);
+};
+
+export default Courses;
